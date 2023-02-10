6b2af92d7ceb46f8862a09374ad519d2231f4bb0
Christopher Foster <cfosprof@gmail.com>
2023-02-10 12:38:04 -0800

    adds function to pass problem 3

--- js/app.js
+++ js/app.js
@@ -52,11 +52,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum1 = (sum(a,b)[0]);
+  let fsum = (sum(sum1, c)[0]);
+  let pro1 = (multiply(a,b)[0]);
+  let fpro = (multiply(pro1, c)[0]);
+  let sstr = `${a} and ${b} and ${c} sum to ${fsum}.`
+  let pstr = `The product of ${a} and ${b} and ${c} is ${fpro}.`
+  return [fsum, fpro, sstr, pstr]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
